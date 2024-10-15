66e6c78a39bf55f6c9daf87c61e7c6a9b0fa5dc8
kishore <kishoreboligarla@gmail.com>
2024-10-15 18:47:00 +0530

    dd another line of code

--- 02-installation.sh
+++ 02-installation.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 USERID=$(id -u)
 echo "user id is: $USERID"
-if [ $USERID -ne 0 ]
-then
-echo "to install mysql give the root acess"
+ if [ $USERID -ne 0 ]
+ then
+   echo "to install mysql give the root acess"
+   exit 1
 fi
-dnf list installed git
+
  
