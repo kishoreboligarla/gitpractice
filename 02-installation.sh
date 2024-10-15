78c484d8334a319f0c30768e6a5a63f1ffb082bb
kishore <kishoreboligarla@gmail.com>
2024-10-15 19:03:34 +0530

    made changes

--- 02-installation.sh
+++ 02-installation.sh
@@ -1,11 +1,19 @@
 #!bin/bash
 USERID=$(id -u)
 echo "user id is: $USERID"
- if [ $USERID -ne 0 ]
- then
-   echo "to install mysql give the root acess"
+  if [ $USERID -ne 0 ]
+  then
+       echo "to install mysql give the root acess"
+      exit 1
+  fi
+dnf list install git 
+  if[ $? -ne 0]
+  then 
+     echo "git not installed going to install.."
+     dnf install git -y
+   else
+   echo"git already installed nothing to do"
 fi
-dnf install git -y
 
 
  
