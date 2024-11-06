ea995b7f5e95ae3d635c97453c86adf90736c114
kishore <kishoreboligarla@gmail.com>
2024-11-06 10:31:47 +0530

    i made changes in code

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,4 +1,5 @@
 #!bin/bash
-PERSION1=RAMESH PERSION2=SURESH
-echo: $PERSION1 HELLO $PERSION2 how r u
-echo: $PERSION2 iam good $PERSION1 what about u
+PERSION1=RAMESH 
+PERSION2=SURESH
+echo " $PERSION1 HELLO $PERSION2 how r u"
+echo "$PERSION2 iam good $PERSION1 what about u"
