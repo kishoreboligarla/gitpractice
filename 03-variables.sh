ba02b3553a27504f287df4e2d2525f347d6786ed
kishore <kishoreboligarla@gmail.com>
2024-11-06 10:39:25 +0530

    i made changes in code

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 PERSION1=Ramseh
 PERSION2=Suresh
-echo " $PERSION1 Hello $PERSION2 how r u"
-echo "$PERSION2 iam good $PERSION1 what about u"
+echo " $PERSION1:: Hello $PERSION2 how r u"
+echo "$PERSION2:: iam fine $PERSION1 how r u doing"
+echo "$PERSION1:: iam doing good $PERSION2 whats going on"
+echo "$PERSION2 :: iam learning shell scrpting $PERSION1"
+
