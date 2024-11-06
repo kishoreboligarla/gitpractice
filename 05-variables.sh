9063aca796f572b5511c6b9c2fbb14ef8e002517
kishore <kishoreboligarla@gmail.com>
2024-11-06 11:14:59 +0530

    i creat a sample code using read and -s command in variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-echo "enter your username"
-read -s USERNAME  #here <-scommand is used for to take text as hidden>, read command is used for take the username at run time
+echo "enter your username::"
+read -s USERNAME  # here <-scommand is used for to take text as hidden>, read command is used for take the username at run time
 echo "username entered is ::$USERNAME"
 echo " enter your password" 
 read -s PASSWORD
