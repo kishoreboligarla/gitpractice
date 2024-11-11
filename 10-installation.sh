de697e846e27b8369ccae85390c043b5c4ce277e
kishore <kishoreboligarla@gmail.com>
2024-11-11 20:06:18 +0530

    install mysql throuh shell script

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,4 +7,10 @@ USERID=$(id -u)
      echo " give the root access"
      exit 1
  fi
- dnf install git -y
+ dnf installed git 
+   if [ $? -ne 0 ]
+     then echo " mysql not installed going to install"
+       dnf install git -y
+ else
+     echo "git is already installed nothing to do"
+ fi       
