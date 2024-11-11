20f5064fb545825ad929a4aab8b099040b42b532
kishore <kishoreboligarla@gmail.com>
2024-11-11 16:56:26 +0530

    i created a code using arrays

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,7 +1,7 @@
 #!bin/bash
  fruits=("kiwi" "apple" "orange" "banana")
    echo first fruit is: "${fruits[0]}"
-   echo second fruit is: "${fruit[1]}"
-   echo second fruit is: "${fruit[2]}"
-   echo second fruit is: "${fruit[3]}"
+   echo second fruit is: "${fruits[1]}"
+   echo second fruit is: "${fruits[2]}"
+   echo second fruit is: "${fruits[3]}"
    echo all fruits are: "${fruits[@]}"      
