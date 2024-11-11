a4b524a6a99675182b4471fc1d8dd778465d320c
kishore <kishoreboligarla@gmail.com>
2024-11-11 17:51:57 +0530

    creat a code using special variables

---
+++ 08-specialvars.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "all variables passed to the script: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "current working directory: $PWD"
+echo "home directory of current user" $HOME"
+echo "PID of the script excuting now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
