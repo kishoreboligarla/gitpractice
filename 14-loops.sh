5eb617d9eacb0ede98a36402ba59737a23e5ddee
kishore <kishoreboligarla@gmail.com>
2024-11-12 15:57:53 +0530

    creat a code using loops

---
+++ 14-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+     echo "run the script with rrot priviliges"
+     exit1
+     fi
+}
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+    echo "$2 is failure"
+    else 
+    echo "$2 is sucess"
+    exit1
+    fi
+}
+CHECK_ROOT
+  for package in $@
+   do
+      dnf installed list $package
+     if [ $? -ne 0 ]
+      then
+       echo "$package is not installed, going to install it.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+      else
+        echo "$package is already installed..nothing to do"
+      fi
+   done
