3a244a1d538348b01d78a83ab01770191541fd65
kishore <kishoreboligarla@gmail.com>
2024-11-12 21:09:55 +0530

    create a code using functions

--- 11-function.sh
+++ 11-function.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-
-    USERID=$(id -u) 
-    echo "user id is:$USERID"
-  
-  VALIDATE()
-       {
-          if [ $1 -ne 0 ]
-          then 
-           echo "$2 is not installed going to install it.."
-          
-         else
-           echo"$2 is alreay installed ... nothing to do"
-           fi
-       }
-        if [ $USERID -ne 0 ]
-            then 
-               echo "give the root priviliges"
-               exit 1
-               fi 
-          dnf install mysql
-         VALIDATE $? mysql
-            dnf install mysql -y
-         dnf installed git
-          VALIDATE $? git
-            dnf install git -y   
-            
-     
+USERID=$(id -u)
+CHECK_ROOT(){
+  if [ $USERID -ne 0 ]
+  then 
+  echo "run the script with root access"
+}
+ VALIDATE(){
+  if [ $1 -ne 0 ]
+  then 
+  echo "$2 is failed"
+  exit 1
+  fi
+  else 
+  echo "$2 is sucess"
+ }
+  dnf installed git
+  if [ $? -ne o ]
+   then 
+    echo " git is not installed going to install it"
+    dnf install git
+    VALIDATE $? git installing
+    else 
+    echo "git alredy intalled nothing todo."
+    dnf installedmysql git
+  if [ $? -ne o ]
+   then 
+    echo " mysql is not installed going to install it"
+    dnf install mysql
+    VALIDATE $? mysql installing
+    else 
+    echo "mysql alredy intalled nothing todo."
+    
