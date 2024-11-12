7d9862be3db74dc89a121e9ced35421f52876b05
kishore <kishoreboligarla@gmail.com>
2024-11-12 21:14:32 +0530

    create a code using functions

--- 11-function.sh
+++ 11-function.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 CHECK_ROOT(){
-  if [ $USERID -ne 0 ]
-  then 
-  echo "run the script with root access"
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
 }
- VALIDATE(){
-  if [ $1 -ne 0 ]
-  then 
-  echo "$2 is failed"
-  exit 1
-  fi
-  else 
-  echo "$2 is sucess"
- }
-  dnf installed git
-  if [ $? -ne o ]
-   then 
-    echo " git is not installed going to install it"
-    dnf install git
-    VALIDATE $? git installing
-    else 
-    echo "git alredy intalled nothing todo."
-    dnf installedmysql git
-  if [ $? -ne o ]
-   then 
-    echo " mysql is not installed going to install it"
-    dnf install mysql
-    VALIDATE $? mysql installing
-    else 
-    echo "mysql alredy intalled nothing todo."
-    
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already installed..nothing to do"
+fi
