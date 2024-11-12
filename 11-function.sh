a83a90d1f107545c08d1cbada84fa749deab6126
kishore <kishoreboligarla@gmail.com>
2024-11-12 21:30:37 +0530

    create a code using functions

--- 11-function.sh
+++ 11-function.sh
@@ -1,45 +1,26 @@
 #!/bin/bash
-
-USERID=$(id -u)
-
+USERID=$(ID -U)
 CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root priveleges"
-        exit 1
-    fi
+  if [ USERID -ne 0 ]
+  then 
+  echo"run the script with root priviliges"
 }
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2 is...FAILED"
-        exit 1
-    else
-        echo "$2 is...SUCCESS"
-    fi
+  if [ $1 -ne 0 ]
+  then
+  echo"$2 failled"
+  exit1
+  fi
+  else
+  echo "$2 success"
 }
-
-CHECK_ROOT
-
-dnf list installed git
-
-if [ $? -ne 0 ]
-then
-    echo "Git is not installed, going to install it.."
-    dnf install git -y
-    VALIDATE $? "Installing Git"
-else
-    echo "Git is already installed, nothing to do.."
-fi
-
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-    echo "MySQL is not installed...going to install"
-    dnf install mysql -y
-    VALIDATE $? "Installing MySQL"
-else
-    echo "MySQL is already installed..nothing to do"
-fi
+ CHECK_ROOT
+ dnf installed mysql
+  if [ $? -ne 0 ]
+  then
+  echo "mysql is not installed going to install"
+  dnf install mysql -y
+  VALIDATE $? mysqlinstalling
+  else 
+   echo"mysql is installed nothing to do"
