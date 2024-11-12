ad7dda55e5d88fc0ef054bea987015e54fb54b9c
kishore <kishoreboligarla@gmail.com>
2024-11-12 21:36:32 +0530

    create a code using functions

--- 11-function.sh
+++ 11-function.sh
@@ -4,13 +4,15 @@ CHECK_ROOT(){
   if [ $USERID -ne 0 ]
   then 
   echo"run the script with root priviliges"
-}
+  exit 1
+  fi
+    }
 
 VALIDATE(){
   if [ $1 -ne 0 ]
   then
   echo"$2 failled"
-  exit1
+  exit 1
   fi
   else
   echo "$2 success"
