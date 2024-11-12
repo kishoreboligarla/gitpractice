5380938545a7cdc8e7f109a1898b25198791274e
kishore <kishoreboligarla@gmail.com>
2024-11-12 09:55:39 +0530

    creating acode using functions

--- 11-function.sh
+++ 11-function.sh
@@ -3,17 +3,24 @@
 
     USERID=$(id -u) 
     echo "user id is:$USERID"
-   #  $USERID( -ne 0 )
-   #    then 
-   #    echo "give the root priviliges"
-     
+  
   VALIDATE()
        {
           if [ $1 -ne 0 ]
           then 
            echo "$2 is not installed going to install it.."
-           dnf install mysql -y
+          
          else
-           echo"mysql is alreay installed ... nothing to do"
+           echo"$2 is alreay installed ... nothing to do"
            fi
        }
+        if [ $USERID -ne 0 ]
+            then 
+               echo "give the root priviliges"
+               exit 1
+               fi 
+         dnf installed mysql
+         VALIDATE $? mysql
+            dnf install mysql -y
+            
+     
