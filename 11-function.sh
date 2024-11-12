a8bd42f861ad022ec809d65e40665a4b44a2b844
kishore <kishoreboligarla@gmail.com>
2024-11-12 08:42:30 +0530

    creating acode using functions

--- 11-function.sh
+++ 11-function.sh
@@ -4,18 +4,15 @@ ROOT_CHECK()
  {
     USERID=(id -u)
     echo "user id is:$USERID"
-    then 
-     echo "$1 is not installed going to install.."
-    else
-     echo " give the root priviliges to install it"
+     
  }
-VALIDATION()
-   {
-    echo $1 -ne 0
-    then 
-       echo " $2 is failed.."
-       exit 1
-    else
-       echo " $2 is sucess"
-    fi
-   } 
+# VALIDATION()
+#    {
+#     echo $1 -ne 0
+#     then 
+#        echo " $2 is failed.."
+#        exit 1
+#     else
+#        echo " $2 is sucess"
+#     fi
+#    } 
