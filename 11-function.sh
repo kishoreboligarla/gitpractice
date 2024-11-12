f7287527493fc23557a23f8a5e4e813168b190b7
kishore <kishoreboligarla@gmail.com>
2024-11-12 09:38:40 +0530

    creating acode using functions

--- 11-function.sh
+++ 11-function.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# ROOT_CHECK()
-#  {
+
     USERID=$(id -u) 
     echo "user id is:$USERID"
     USERID(ne -0)
       then 
       echo "give the root priviliges"
      
-#  }
-# VALIDATION()
-#    {
-#     echo $1 -ne 0
-#     then 
-#        echo " $2 is failed.."
-#        exit 1
-#     else
-#        echo " $2 is sucess"
-#     fi
-#    } 
+  VALIDATE()
+       {
+          if [ $1 -ne 0 ]
+          then 
+           echo "$2 is not installed going to install it.."
+           dnf install mysql -y
+         else
+           echo"mysql is alreay installed ... nothing to do"
+           fi
+       }
