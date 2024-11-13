180a1a198bf884cb821e38d13b6c13969c57eb59
kishore <kishoreboligarla@gmail.com>
2024-11-13 19:05:31 +0530

    records a logs for a code

--- 16-redirectories.sh
+++ 16-redirectories.sh
@@ -4,7 +4,7 @@
   G="\e[32m"
   Y="\e[33m"
   N="\e[0m"
-  LOGS_FOLDER="/var/logs/shell-script"
+  LOGS_FOLDER="/var/log/shell-script"
   SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
   TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
   LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
