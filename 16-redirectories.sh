0c32a5210ccde18537d3637896e903fc7e2713c5
kishore <kishoreboligarla@gmail.com>
2024-11-13 16:16:33 +0530

    recors a logs foe a code

--- 16-redirectories.sh
+++ 16-redirectories.sh
@@ -5,10 +5,10 @@
   Y="\e[33m"
   N="\e[0m"
   LOGS_FOLDER="/var/logs/shell-script"
-  SCRIPT_NAME"$(echo $0 | cut -d "." -f1)
+  SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
   TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
   LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-  mkdir -p $LOGS_FOLDER
+  mkdir -p LOGS_FOLDER
   CHECK_ROOT(){
       if [ $USERID -ne 0 ]
       then
@@ -17,7 +17,7 @@
       fi
  
   VALIDATE(){
-      if [ $? -ne 0 ]
+      if [ $1 -ne 0 ]
       then
       echo "$2 is $R failure $N">>&$LOG_FILE
       exit 1
